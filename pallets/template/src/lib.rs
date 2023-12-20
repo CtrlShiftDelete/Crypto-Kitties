2af057cd34c8640e42bd6eae99aa13e64fda51bf
Toppo96 <mukesh96toppo@gmail.com>
2023-12-20 11:47:47 +0530

    transfer

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -51,6 +51,24 @@ pub mod pallet {
 		pub owner: T::AccountId,
 	}
 
+	/// Keeps track of the number of kitties in existence.
+	#[pallet::storage]
+	pub(super) type CountForKitties<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+	/// Maps the kitty struct to the kitty DNA.
+	#[pallet::storage]
+	pub(super) type Kitties<T: Config> = StorageMap<_, Twox64Concat, [u8; 16], Kitty<T>>;
+
+	/// Track the kitties owned by each account.
+	#[pallet::storage]
+	pub(super) type KittiesOwned<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::AccountId,
+		BoundedVec<[u8; 16], T::MaxKittiesOwned>,
+		ValueQuery,
+	>;
+
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -69,15 +87,166 @@ pub mod pallet {
 		type KittyRandomness: Randomness<Self::Hash, Self::BlockNumber>;
 	}
 
+	// Your Pallet's events.
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {}
-	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Event<T: Config> {
+		/// A new kitty was successfully created.
+		Created { kitty: [u8; 16], owner: T::AccountId },
+		/// A kitty was successfully transferred.
+		Transferred { from: T::AccountId, to: T::AccountId, kitty: [u8; 16] },
+	}
 
+	// Your Pallet's error messages.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// An account may only own `MaxKittiesOwned` kitties.
+		TooManyOwned,
+		/// This kitty already exists!
+		DuplicateKitty,
+		/// An overflow has occurred!
+		Overflow,
+		/// This kitty does not exist!
+		NoKitty,
+		/// You are not the owner of this kitty.
+		NotOwner,
+		/// Trying to transfer or buy a kitty from oneself.
+		TransferToSelf,
+	}
+
+	// Your Pallet's callable functions.
 	#[pallet::call]
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		/// Create a new unique kitty.
+		///
+		/// The actual kitty creation is done in the `mint()` function.
+		#[pallet::weight(0)]
+		// #[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
+			// Make sure the caller is from a signed origin
+			let sender = ensure_signed(origin)?;
+
+			// Generate unique DNA and Gender using a helper function
+			let (kitty_gen_dna, gender) = Self::gen_dna();
+
+			// Write new kitty to storage by calling helper function
+			Self::mint(&sender, kitty_gen_dna, gender)?;
+
+			Ok(())
+		}
+
+		/// Directly transfer a kitty to another recipient.
+		///
+		/// Any account that holds a kitty can send it to another Account. This will reset the
+		/// asking price of the kitty, marking it not for sale.
+		#[pallet::weight(0)]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			to: T::AccountId,
+			kitty_id: [u8; 16],
+		) -> DispatchResult {
+			// Make sure the caller is from a signed origin
+			let from = ensure_signed(origin)?;
+			let kitty = Kitties::<T>::get(&kitty_id).ok_or(Error::<T>::NoKitty)?;
+			ensure!(kitty.owner == from, Error::<T>::NotOwner);
+			Self::do_transfer(kitty_id, to)?;
+			Ok(())
+		}
+	}
 
 	// Your Pallet's internal functions.
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		// Generates and returns DNA and Gender
+		fn gen_dna() -> ([u8; 16], Gender) {
+			// Create randomness
+			let random = T::KittyRandomness::random(&b"dna"[..]).0;
+
+			// Create randomness payload. Multiple kitties can be generated in the same block,
+			// retaining uniqueness.
+			let unique_payload = (
+				random,
+				frame_system::Pallet::<T>::extrinsic_index().unwrap_or_default(),
+				frame_system::Pallet::<T>::block_number(),
+			);
+
+			// Turns into a byte array
+			let encoded_payload = unique_payload.encode();
+			let hash = frame_support::Hashable::blake2_128(&encoded_payload);
+
+			// Generate Gender
+			if hash[0] % 2 == 0 {
+				(hash, Gender::Male)
+			} else {
+				(hash, Gender::Female)
+			}
+		}
+
+		// Helper to mint a kitty
+		pub fn mint(
+			owner: &T::AccountId,
+			dna: [u8; 16],
+			gender: Gender,
+		) -> Result<[u8; 16], DispatchError> {
+			// Create a new object
+			let kitty = Kitty::<T> { dna, price: None, gender, owner: owner.clone() };
+
+			// Check if the kitty does not already exist in our storage map
+			ensure!(!Kitties::<T>::contains_key(&kitty.dna), Error::<T>::DuplicateKitty);
+
+			// Performs this operation first as it may fail
+			let count = CountForKitties::<T>::get();
+			let new_count = count.checked_add(1).ok_or(Error::<T>::Overflow)?;
+
+			// Append kitty to KittiesOwned
+			KittiesOwned::<T>::try_append(&owner, kitty.dna)
+				.map_err(|_| Error::<T>::TooManyOwned)?;
+
+			// Write new kitty to storage
+			Kitties::<T>::insert(kitty.dna, kitty);
+			CountForKitties::<T>::put(new_count);
+
+			// Deposit our "Created" event.
+			Self::deposit_event(Event::Created { kitty: dna, owner: owner.clone() });
+
+			// Returns the DNA of the new kitty if this succeeds
+			Ok(dna)
+		}
+
+		// Update storage to transfer kitty
+        pub fn do_transfer(
+            kitty_id: [u8; 16],
+            to: T::AccountId,
+        ) -> DispatchResult {
+            // Get the kitty
+            let mut kitty = Kitties::<T>::get(&kitty_id).ok_or(Error::<T>::NoKitty)?;
+            let from = kitty.owner;
+
+            ensure!(from != to, Error::<T>::TransferToSelf);
+            let mut from_owned = KittiesOwned::<T>::get(&from);
+
+            // Remove kitty from list of owned kitties.
+            if let Some(ind) = from_owned.iter().position(|&id| id == kitty_id) {
+                from_owned.swap_remove(ind);
+            } else {
+                return Err(Error::<T>::NoKitty.into())
+            }
+
+            // Add kitty to the list of owned kitties.
+            let mut to_owned = KittiesOwned::<T>::get(&to);
+            to_owned.try_push(kitty_id).map_err(|_| Error::<T>::TooManyOwned)?;
+
+            // Transfer succeeded, update the kitty owner and reset the price to `None`.
+            kitty.owner = to.clone();
+            kitty.price = None;
+
+            // Write updates to storage
+            Kitties::<T>::insert(&kitty_id, kitty);
+            KittiesOwned::<T>::insert(&to, to_owned);
+            KittiesOwned::<T>::insert(&from, from_owned);
+
+            Self::deposit_event(Event::Transferred { from, to, kitty: kitty_id });
+
+            Ok(())
+        }
+    }
 }
